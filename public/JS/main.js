e2267b68a686d1f4b78273a31d62620ca12d774a
Luis Andrade reuss <landrade@MacBook-Pro-de-Luis.local>
2017-07-24 13:31:52 +0700

    mondey

--- public/JS/main.js
+++ public/JS/main.js
@@ -2,22 +2,54 @@
 console.log("Hello");
 alert("Hello");
 
+//How many hours are there in a year?
+
+var weekHours = 24 * 7;
+var monthHours = weekHours * 4;
+var yearHours = monthHours * 12;
+
+console.log("Hour in a Year : " + yearHours);
+
+//How many minutes are there in a decade?
+
+var MinutesDay = 60 * 24;
+var MinutesMonth = MinutesDay * 4;
+var MinutesYear = MinutesMonth * 12;
+var MinutesDecade = MinutesYear * 10;
+
+console.log("Minutes in a Decade :"+MinutesDecade);
 
 
+//How many seconds old are you?
+
+var SecondsDay = MinutesDay * 60;
+var SecondsMonth = SecondsDay * 4;
+var SecondsYear = SecondsMonth * 12;
+var SecondsOld = SecondsYear * 36;
+
+console.log("Im "+ SecondsOld + " Seconds old");
+
+//If Bob is 1246 million seconds old, how old is Bob in years?
+
+var SecondsOldBob = 124600000;
+var BobYears = SecondsOldBob / SecondsYear;
+
+console.log("Bob is "+BobYears+" years old");
+
+//-------------------- task2 ---------------------------------
 
 
+var FirstName = prompt("Please enter you firts name")
 
+var SecondName = prompt("Please enter you last name")
 
+function FullName(FirstName,SecondName){
+    console.log( FirstName+" "+ SecondName);
+}
 
+FullName(FirstName,SecondName)
 
-
-
-
-
-
-
-
-
+//-------------------- task4 ----------------------------------
 
 
 
