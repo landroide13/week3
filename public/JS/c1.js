5d3be723dff3d4a5ab2aa93d7ab4171b1fde2a3c
Luis Andrade reuss <landrade@MacBook-Pro-de-Luis.local>
2017-07-24 18:40:40 +0700

    monday

--- public/JS/c1.js
+++ public/JS/c1.js
@@ -1,5 +1,8 @@
+'use strict'
 
-var h1 = document.getElementById("heading")
+
+
+let h1 = document.getElementById("heading")
 
 console.log("h1: ", h1);
 console.log("h1 id ", h1.id);
@@ -9,12 +12,12 @@ console.log("h1 innerText ", h1.innerText);
 
 h1.style.color = "blue"
 
-var h1 = document.getElementsByTagName("h1")[0]
+let h1 = document.getElementsByTagName("h1")[0]
 
 h1.className = "milestone"
 console.log("h1 with class name",h1)
 
-var items = document.getElementsByClassName("item")
+let items = document.getElementsByClassName("item")
 
 console.log("items: ", items);
 /*console.log("item 0: ", items[0]);
@@ -22,13 +25,11 @@ console.log("item 0's innerHTML: ", items[0].innerHTML.toUpperCase());
 console.log("item 1's innerHTML: ", items[1].innerHTML);
 console.log("item 2's innerHTML: ", items[2].innerHTML); */
 
-for (var i = 0 ; i < items.length ; i++){
-  var item = items[i]
+for (let i = 0 ; i < items.length ; i++){
+  let item = items[i]
   console.log(item.innerHTML.toUpperCase())
+
 }
-
-
-
 
 
 
--- public/JS/main.js
+++ public/JS/main.js
@@ -1,47 +1,48 @@
+'use strict'
 
 console.log("Hello");
 alert("Hello");
 
 //How many hours are there in a year?
 
-var weekHours = 24 * 7;
-var monthHours = weekHours * 4;
-var yearHours = monthHours * 12;
+let weekHours = 24 * 7;
+let monthHours = weekHours * 4;
+let yearHours = monthHours * 12;
 
 console.log("Hour in a Year : " + yearHours);
 
 //How many minutes are there in a decade?
 
-var minutesDay = 60 * 24;
-var minutesMonth = minutesDay * 4;
-var minutesYear = minutesMonth * 12;
-var minutesDecade = minutesYear * 10;
+let minutesDay = 60 * 24;
+let minutesMonth = minutesDay * 4;
+let minutesYear = minutesMonth * 12;
+let minutesDecade = minutesYear * 10;
 
 console.log("Minutes in a Decade :"+minutesDecade);
 
 
 //How many seconds old are you?
 
-var secondsDay = minutesDay * 60;
-var secondsMonth = secondsDay * 4;
-var secondsYear = secondsMonth * 12;
-var secondsOld = secondsYear * 36;
+let secondsDay = minutesDay * 60;
+let secondsMonth = secondsDay * 4;
+let secondsYear = secondsMonth * 12;
+let secondsOld = secondsYear * 36;
 
 console.log("Im "+ secondsOld + " Seconds old");
 
 //If Bob is 1246 million seconds old, how old is Bob in years?
 
-var ysBob = 124600000;
-var bobYears = ysBob / secondsYear;
+let ysBob = 124600000;
+let bobYears = ysBob / secondsYear;
 
 console.log("Bob is "+bobYears+" years old");
 
 //-------------------- task2 ---------------------------------
 
 
-var firstName = prompt("Please enter you firts name")
+let firstName = prompt("Please enter you firts name")
 
-var secondName = prompt("Please enter you last name")
+let secondName = prompt("Please enter you last name")
 
 function fullName(firstName,secondName){
     console.log( firstName+" "+ secondName);
@@ -51,19 +52,19 @@ fullName(firstName,secondName)
 
 //-------------------- task4 ----------------------------------
 
-var number = [1,2,3,4,5,6,7,8,9,10]
+let number = [1,2,3,4,5,6,7,8,9,10]
 
 for(i = 0 ; i <= number.length ; i++){
   console.log(number[i])
 }
 
 
-number.forEach(function(i) {
+number.forEach((i)=> {
    console.log(i) 
 }, this);
 
 
-number.forEach(function(i){
+number.forEach((i)=>{
   if(i > 5){
   console.log(i)
   }
@@ -93,7 +94,7 @@ invert(allNumber);
 
 //------------------------ B1 ------------------------------
 
-var myDog = {
+let myDog = {
   "name":"vigo",
   "legs":6,
   "tails":7,
@@ -101,7 +102,7 @@ var myDog = {
 };
 
 
-var rating = {};
+let rating = {};
 
 rating["biber"] = 0;
 rating["Charlie"] = 7;
@@ -114,10 +115,10 @@ console.log(Object.values(rating));
 //------------------------ B2 -------------------------------
 
 
-var justin = {"name":"Justin","age":23}
-var selena = {"name":"Selena","age":24}
-var victor = {"name":"Victor","age":27}
-var james = {"name":"James","age":47}
+let justin = {"name":"Justin","age":23}
+let selena = {"name":"Selena","age":24}
+let victor = {"name":"Victor","age":27}
+let james = {"name":"James","age":47}
 
 function toSentence(obj){
  return `${obj.name} is ${obj.age} years old.`
